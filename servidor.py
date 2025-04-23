407b460bf2299e25cdf6e2fc426e5fedec6b72af
renegadelhaedu <renegadelha.edu@gmail.com>
2025-04-23 12:10:52 -0300

    aula 2304

--- servidor.py
+++ servidor.py
@@ -7,14 +7,28 @@ app = Flask(__name__)
 #criando uma rota endpoint
 @app.route('/')
 def pagina_principal():
-    return render_template('segundodia.html')
+    return render_template('minhasfesta.html')
 
 @app.route('/maisinfo')
 def saber_suino():
     return render_template('terceirodia.html')
 
+@app.route('/verificar')
+def verificar_convidado():
+    #pega o valor do campo que veio da requisição do cliente
+    nome = request.values.get('nomepessoa')
+
+    convidados = ['tevez','cassio','ronaldo','romero']
+
+    #coloca o nome do usuário em minúsculo e compara
+    if nome.lower() in convidados:
+        return render_template('foiconvidado.html')
+    else:
+        return render_template('naofoiconvidado.html')
+
+
 #executando o servidor
-app.run(host='0.0.0.0')
+app.run()
 
 
 
