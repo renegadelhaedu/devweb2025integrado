a485552b7cb1ad134570025f034153c056023f0d
renegadelhaedu <renegadelha.edu@gmail.com>
2025-05-28 17:46:37 -0300

    aula 2805

--- servidorzeroum.py
+++ servidorzeroum.py
@@ -11,6 +11,18 @@ def pagina_inicial():
 def saber_ano():
     idade_user = request.form.get('idade')
     ano = 2025 - int(idade_user)
-    return render_template('anoniver.html', ano_user=ano)
+
+
+
+@app.route('/signo', methods=['post'])
+def verificar_signo():
+    data = request.form.get('datanascimento')
+    mes = int(data.split('-')[1])
+    dia = int(data.split('-')[2])
+    if (dia >= 23 and dia <= 31 and mes == 10) or (dia <=21 and mes==11):
+        return 'esse serumaninho é uma criatura escorpianica. Cuidado'
+
+    else:
+        return  'pelo menos nao é escorpiao'
 
 app.run()
